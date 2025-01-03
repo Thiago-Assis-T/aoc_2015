5d80caeb7d8df17afa68b0b14eab03dc41753526
Thiago Assis <thiago.assisfernandes@gmail.com>
2025-01-03 19:33:12 -0300

    day02 in progress

---
+++ day_02/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char **input = argv;
+	int inputSize = argc;
+
+	// input[1] = 29x13x26
+	// input[2] = 11x11x14
+	return 0;
+}
