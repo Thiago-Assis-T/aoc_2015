03b5f7406e45e086dd266ad78a97b95488b2cce6
Thiago Assis <thiago.assisfernandes@gmail.com>
2025-01-06 09:43:26 -0300

    lol

--- day_02/main.c
+++ day_02/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	char **input = argv;
 	int inputSize = argc;
 
+	printf("%s", input[1]);
 	// input[1] = 29x13x26
 	// input[2] = 11x11x14
 	return 0;
